4b757fc80faf84b442445984a136365bcbefdf7e
oyqx17 <qx.ouyang@gmail.com>
2014-06-26 12:37:53 +0000

    loger

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application.
 SampleApp::Application.initialize!
+
+Rails::Initializer.run do |config|
+  config.active_record.colorize_logging = false
+end
+
+class Logger
+  def format_message(level, time, progname, msg)
+    "#{time.to_s(:db)} #{level} -- #{msg}\n"
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,8 +49,9 @@ SampleApp::Application.configure do
   config.log_tags = [:subdomain, :uuid]
 
   # Use a different logger for distributed setups.
-  config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
+  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.log_level = :debug
+  config.logger = Logger.new(...) # or log4r
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
